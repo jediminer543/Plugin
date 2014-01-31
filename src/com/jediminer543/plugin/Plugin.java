e2c34889a193fd10d7955f0692f5d659b80d7709
jediminer543 <jediminer543@users.noreply.github.com>
2014-01-31 17:22:58 +0000

    Added Command Spawn

--- src/com/jediminer543/plugin/Plugin.java
+++ src/com/jediminer543/plugin/Plugin.java
@@ -5,16 +5,20 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+
+
 public final class Plugin extends JavaPlugin 
 {
 	
     @Override
     public void onEnable(){
-
+    	getLogger().info("onEnable has been invoked!");
+    	this.saveDefaultConfig();
     }
  
     @Override
     public void onDisable() {
+    	getLogger().info("onEnable has been invoked!");
 
     }
     
@@ -22,12 +26,27 @@ public final class Plugin extends JavaPlugin
 	public boolean onCommand(CommandSender sender, Command cmd, String arg2, String[] args)
 	{
 		boolean player = false;
+		Player senderp;
 		if (sender instanceof Player)
 			player = true;
+			senderp = (Player) sender;
 		switch (cmd.getName().toLowerCase())
 		{
 			case "test":
 				sender.sendMessage("Test Sucsesfull");
+			case "sethome":
+			//case "seth":
+				if(player)
+				{
+					
+					sender.sendMessage(senderp.getLocation().toString());
+					sender.sendMessage("Home Set If It Was Implemented");
+				}
+			case "spawn":
+				if(player)
+				{
+					senderp.teleport(senderp.getBedSpawnLocation());
+				}
 			default: break;
 		}
 		return false;
