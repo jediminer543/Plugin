5e34dcb72a2c88ab7a9b6831b459115dbae5365e
jediminer543 <jediminer543@users.noreply.github.com>
2014-03-15 20:52:09 +0000

    Added more stuff

---
+++ src/com/jediminer543/plugin/config/objects/MultiBlock.java
@@ -0,0 +1,8 @@
+package com.jediminer543.plugin.config.objects;
+
+import org.bukkit.Location;
+
+public class MultiBlock
+{
+	public Location CoreLocation;
+}
---
+++ src/com/jediminer543/plugin/config/objects/MultiBlockType.java
@@ -0,0 +1,16 @@
+package com.jediminer543.plugin.config.objects;
+
+public enum MultiBlockType
+{
+	//Generator(),
+	TEST("TEST");
+	
+	 public String guiName;
+	
+	MultiBlockType(String GUIName)
+	{
+		this.guiName = GUIName;
+	}
+	
+	
+}
